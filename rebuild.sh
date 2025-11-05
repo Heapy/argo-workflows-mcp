46f7c012997dae2c5216813116f5fdcac48394d9
Ruslan Ibragimov <ruslan@heapy.io>
2025-11-05 23:20:56 +0300

    Automate versioning in workflows and Docker build process by adding `version.sh`.

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,8 @@
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 25.0.1-amzn
 ./gradlew installDist
-docker build -t ghcr.io/heapy/argo-workflows-mcp:main .
+VERSION="$(./version.sh)"
+docker build --build-arg MCP_SERVER_VERSION="$VERSION" \
+  -t ghcr.io/heapy/argo-workflows-mcp:main \
+  -t ghcr.io/heapy/argo-workflows-mcp:"$VERSION" \
+  .
