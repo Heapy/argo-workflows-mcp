f70a7d01379a104f1c7244bf9504dc3024f7c524
Ruslan Ibragimov <ruslan@heapy.io>
2025-11-05 19:05:11 +0300

    Update `rebuild.sh` to use GitHub Container Registry for Docker image

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,4 @@
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 25.0.1-amzn
 ./gradlew installDist
-docker build -t argo-workflows-mcp:latest .
+docker build -t ghcr.io/heapy/argo-workflows-mcp:main .
